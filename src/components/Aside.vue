91fed52c80d093dd6c9a551d97bee10c90fab66b
NuBreed <ayodejiadio@gmail.com>
2023-07-24 18:25:07 +0200

    sidebar css

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -8,7 +8,7 @@ import Sidebar_links from './Sidebar_links.vue'
         <font-awesome-icon :icon="['fas', 'xmark']" class="fas" />
       </div>
       <section class="aside-center">
-        <Sidebar_links job="peter ajayi" />
+        <Sidebar_links job="peter ajayi" class="links" />
         <Sidebar_links job="peter ajayi" />
         <Sidebar_links job="peter ajayi" />
         <Sidebar_links job="peter ajayi" />
@@ -36,18 +36,28 @@ import Sidebar_links from './Sidebar_links.vue'
   position: relative;
   display: flex;
   flex-direction: column;
+  justify-content: space-evenly;
   gap: 24px;
+  width: 100%;
+  height: 100%;
 }
 .aside-header {
   position: absolute;
   right: 12px;
-  top: 12px;
-  margin-bottom: 240px;
+  top: 8px;
+  margin-bottom: 24px;
 }
 .fas {
   font-size: 42px;
 }
-.aside-center {
-  background-color: red;
+.fas:hover {
+  color: red;
+}
+.links {
+  border-radius: 4px;
+}
+.links:hover {
+  margin-bottom: 12px;
+  box-shadow: 0px 1px 2px #ffffff;
 }
 </style>
